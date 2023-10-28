b8e0dbee14246784ad281c9f5587bd4f70a8460f
Matheus C. O. Santos <42250854+matheuscosantos@users.noreply.github.com>
2023-10-28 20:02:02 -0300

    altera main.tf

--- main.tf
+++ main.tf
@@ -13,19 +13,3 @@ resource "aws_db_instance" "database" {
   parameter_group_name = "default.postgres11"
   skip_final_snapshot  = true
 }
-
-data "aws_secretsmanager_secret" "db_username" {
-  name = "db_username"
-}
-
-data "aws_secretsmanager_secret_version" "db_username" {
-  secret_id = data.aws_secretsmanager_secret.db_username.id
-}
-
-data "aws_secretsmanager_secret" "db_password" {
-  name = "db_password"
-}
-
-data "aws_secretsmanager_secret_version" "db_password" {
-  secret_id = data.aws_secretsmanager_secret.db_password.id
-}
